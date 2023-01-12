d20b845c6775d71d6d55f1a7d62953eda72a9cf2
emerson <emersonfilho1@hotmail.com>
2023-01-12 18:53:24 -0300

    route,controller  and view of register page created

---
+++ crud-users-laravel-8/resources/views/auth/register.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register page</title>
+</head>
+
+<body>
+
+    <h1>Register User</h1>
+    <br>
+    <form action="" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="name">Digite seu Nome Completo</label>
+            <input type="text" class="form-control" placeholder="Digite seu Nome Completo" name="name">
+
+            <br><br>
+            <div class="form-group">
+                <label for="email">email</label>
+                <input type="email" placeholder="Digite seu email" name="email">
+
+                <br><br>
+                <div class="form-group">
+                    <label for="cpf">CPF</label>
+                    <input type="text" placeholder="Digite seu CPF" name="cpf">
+                </div>
+                <br><br>
+                <div class="form-group">
+                    <label for="cep">CEP</label>
+                    <input type="text" placeholder="Digite seu CEP" name="cep">
+                </div>
+
+                <br><br>
+
+                <div class="form-group">
+                    <label for="cidade">Cidade</label>
+                    <input type="text" placeholder="Digite sua Cidade" name="cidade">
+                </div>
+                <br><br>
+                <div class="form-group">
+                    <label for="estado">Digite seu Estado</label>
+                    <input type="text" placeholder="Digite seu Estado" name="estado">
+                </div>
+                <br><br>
+                <div class="form-group">
+                    <label for="password">Digite sua senha</label>
+                    <input type="password" placeholder="Digite sua senha" name="password">
+                </div>
+                <input type="submit" value="Salvar">
+    </form>
+</body>
+
+</html>
