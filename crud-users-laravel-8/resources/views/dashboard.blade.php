577b90e10af17e95eeff9535ccd73272a00aeb81
emerson <emersonfilho1@hotmail.com>
2023-01-13 02:03:47 -0300

    rotas, controller e view  para pagina de listagem do usuario criadas

---
+++ crud-users-laravel-8/resources/views/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard page</title>
+</head>
+
+<body>
+    @csrf
+    <h1>Dashboard</h1>
+    <br>
+    <table>
+        <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $data->name }}</td>
+                <td>{{ $data->email }}</td>
+                <td><a href="logout">Logout</a></td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
