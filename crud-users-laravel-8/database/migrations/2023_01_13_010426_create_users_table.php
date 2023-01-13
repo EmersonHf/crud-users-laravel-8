59ad4746c76debb74d7ddedc02eafa727b5b1cde
emerson <emersonfilho1@hotmail.com>
2023-01-12 22:16:35 -0300

    pagina de login criada

--- crud-users-laravel-8/database/migrations/2023_01_13_010426_create_users_table.php
+++ crud-users-laravel-8/database/migrations/2023_01_13_010426_create_users_table.php
@@ -15,10 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('name');
-            $table->string('cpf')->unique();
-            $table->string('password');
             $table->timestamps();
         });
     }
