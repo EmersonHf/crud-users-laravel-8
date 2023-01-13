87f6e6e51b4ccd3c899063364ddca1f45b1cb072
emerson <emersonfilho1@hotmail.com>
2023-01-13 01:32:50 -0300

    rota do login criada, função no loginController para logar.validaçoes no login.blade  ja implementadas. tratamento de erros também  implementados no login.blade

--- crud-users-laravel-8/app/Http/Controllers/LoginController.php
+++ crud-users-laravel-8/app/Http/Controllers/LoginController.php
@@ -3,10 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
+use App\Models\User;
+use Hash;
 
 class LoginController extends Controller
 {
     public function login(){
         return view('auth.login');
+    }
+
+    public function loginUser(Request $request){
+        $request->validate([
+
+            'email' => 'required|email',
+            'password'=>'required|min:6|max:20',
+
+           ]);
+           $user = User::where('email','=',$request->email)->first();
+           if($user){
+            if(Hash::check($request->password, $user->password)){
+                $request->session()->put('loginId',$user->id);
+                return redirect('dashboard');
+            }else{
+                return back()->with('falha','senha incorreta');
+            }
+           }else{
+            return back()->with('falha','this email is not registered');
+           }
     }
 }
